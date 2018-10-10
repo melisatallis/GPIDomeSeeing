9aa06e613bde42bec9287c57552186691b4a2167
melisatallis <melisatallis@gmail.com>
2018-10-10 16:32:29 -0700

    add comments

--- scripts/process_gpi_phase.py
+++ scripts/process_gpi_phase.py
@@ -7,11 +7,20 @@ import copy
 from os.path import join, splitext
 
 def main(datafile, directory, save_images = True):
-    """Perform spatial power spectrum analysis of GPI phase and save data products to a     specific subdirectory.
+    """
+    #################
+    Melisa Tallis - 2018-10-10
+    Do a spatial frequency power analysis of GPI phase and save data products to specific directory.
 
-    Args:
-        datafile (str): GPI reduced data file name.
-        directory (str): Name of main directory to build subdirectory tree in.
-        save_images (bool): Specify whether to save graph images.
+    Inputs:
+        datafile    - (str) GPI reduced data file name.
+        directory   - (str) Name of main directory to build subdirectory tree in.
+        
+    Flags:
+        telescope   - (str) Parameters for GPI telescope
+        dept        - (bool) depiston and detilt the output and impose the aperture
+        phmicrons   - (bool) convert phase from radians to microns at 500 nm
+        save_images - (bool): PSD plots
+        outdir      - change to save output files to a directory other than current
     """
     file_dict 
